f0c5184ed184097dd5e3bbe2abc74e925f6b1010
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-26 22:42:26 +0200

    Add Layer for db and ApplicationDbContext.cs

---
+++ AppData/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppData.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=MobileWorld;Integrated Security=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
