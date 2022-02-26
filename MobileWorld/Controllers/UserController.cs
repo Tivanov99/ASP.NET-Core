b6295b3ee37d94b307c07eba8cb162e8e5268433
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-26 12:28:19 +0200

    Added Register.cshtml and register.css

--- MobileWorld/Controllers/UserController.cs
+++ MobileWorld/Controllers/UserController.cs
@@ -8,5 +8,10 @@ namespace MobileWorld.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
