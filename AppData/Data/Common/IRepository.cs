139631f8d37aa947f5d23fa5c0d59fab85b7dce1
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-26 22:57:01 +0200

    Add Repository.cs

---
+++ AppData/Data/Common/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AppData.Data.Common
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+
+        IQueryable<T> All<T>() where T : class;
+
+
+        int SaveChanges();
+    }
+}
---
+++ AppData/Data/Common/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace AppData.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public Repository(ApplicationDbContext _context)
+        {
+            this.dbContext = _context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            DbSet<T>().Add(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return this.DbSet<T>().AsQueryable();
+        }
+
+        public int SaveChanges()
+        {
+            return this.dbContext.SaveChanges();
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return dbContext.Set<T>();
+        }
+    }
+}
