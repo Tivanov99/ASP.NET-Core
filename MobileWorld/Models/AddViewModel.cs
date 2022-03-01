ef9969e463817220d7aadf5cc05662574fadf6ba
Tivanov99 <trifon.ivanov21@gmail.com>
2022-03-02 01:14:56 +0200

    Updated CarView.css

---
+++ MobileWorld/Models/AddViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MobileWorld.Models
+{
+    public class AddViewModel
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
