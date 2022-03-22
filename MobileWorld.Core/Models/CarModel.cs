41c77b921a31e694bdb1185d82b800019d786d09
Tivanov99 <trifon.ivanov21@gmail.com>
2022-03-22 18:56:03 +0200

    Update views models

--- MobileWorld.Core/Models/CarModel.cs
+++ MobileWorld.Core/Models/CarModel.cs
@@ -4,6 +4,8 @@
     {
         public string Color { get; set; }
 
+        public int HorsePower { get; set; }
+
         public int SeatsCount { get; set; }
 
         public decimal Mileage { get; set; }
