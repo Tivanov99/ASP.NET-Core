8468b0c329fdafe1c54c81307ae6c701357034d3
Tivanov99 <trifon.ivanov21@gmail.com>
2022-03-22 17:58:35 +0200

    Remove unused folders

---
+++ MobileWorld.Core/ViewModels/AdViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MobileWorld.Core.Models;
+
+namespace MobileWorld.Core.ViewModels
+{
+    public class AdViewModel
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public CarModel Car { get; set; }
+
+        public OwnerModel Owner { get; set; }
+
+        public RegionModel Region { get; set; }
+
+        public FeaturesModel Features { get; set; } = new();
+    }
+}
