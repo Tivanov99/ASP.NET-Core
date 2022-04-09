aee2b3bbf223dc895f129c8709d2aa6b9f3acf98
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-09 13:35:01 +0300

    Update Register.cshtml.cs

--- MobileWorld/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MobileWorld/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,8 +120,12 @@ namespace MobileWorld.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
