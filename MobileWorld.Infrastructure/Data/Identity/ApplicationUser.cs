8af4e5eaa83665bb4d9c5c026337924c3a323008
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-09 12:16:58 +0300

    Add new properties to ApplicationUser.cs plus validation attributes

--- MobileWorld.Infrastructure/Data/Identity/ApplicationUser.cs
+++ MobileWorld.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using MobileWorld.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MobileWorld.Infrastructure.Data.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(40,ErrorMessage ="Потребителското име трябва да е между 5 и {0} символа")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "Фамилното име трябва да е между 5 и {0} символа")]
+        public string LastName { get; set; }
+
         public List<Ad> Ads { get; set; } = new();
 
         public List<FavoriteAd> FavoriteAds { get; set; } = new();
