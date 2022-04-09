548c7e4720bfc08501347556f843f3c20c711345
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-09 16:14:10 +0300

    Update CreateAd.cshtml

--- MobileWorld/Controllers/HomeController.cs
+++ MobileWorld/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using MobileWorld.Core.Contracts;
 using MobileWorld.Core.ViewModels;
 using System.Diagnostics;
