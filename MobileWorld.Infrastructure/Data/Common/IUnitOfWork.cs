ab253ee4628f8ffcf44d266d8a29fb8f45213ada
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-10 15:12:27 +0300

    Update IUnitOfWork.cs, UnitOfWork.cs

---
+++ MobileWorld.Infrastructure/Data/Common/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using MobileWorld.Infrastructure.Data.Models;
+
+namespace MobileWorld.Infrastructure.Data.Common
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Ad> AdRepository { get;  }
+
+        IRepository<Car> CarRepository { get;  }
+
+        void Save();
+
+        //IRepository<Engine> EngineRepository { get; set; }
+
+    }
+}
---
+++ MobileWorld.Infrastructure/Data/Common/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using MobileWorld.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileWorld.Infrastructure.Data.Common
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private ApplicationDbContext context ;
+
+        public UnitOfWork(ApplicationDbContext _context,
+            IRepository<Ad> _adRepository
+            , IRepository<Car> _carRepository)
+        {
+            this.context = _context;
+            this.AdRepository = _adRepository;
+            this.CarRepository = _carRepository;
+        }
+
+
+        public IRepository<Ad> AdRepository { get ;  }
+
+        public IRepository<Car> CarRepository { get ;}
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
