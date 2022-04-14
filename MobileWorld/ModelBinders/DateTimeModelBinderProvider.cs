dd36f1d3a45185fa3785d8557ca940cf260e8d61
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-14 21:43:06 +0300

    Added DateTimeModelBinderProvider.cs

---
+++ MobileWorld/ModelBinders/DateTimeModelBinderProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MobileWorld.ModelBinders
+{
+    public class DateTimeModelBinderProvider : IModelBinderProvider
+    {
+        private readonly string _dateTimeFormat;
+        public DateTimeModelBinderProvider(string dateTimeFormat)
+        {
+            _dateTimeFormat = dateTimeFormat;
+        }
+
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(DateTime) || context.Metadata.ModelType == typeof(DateTime?))
+            {
+                return new DateTimeModelBinder(_dateTimeFormat);
+            }
+            return null;
+        }
+    }
+}
