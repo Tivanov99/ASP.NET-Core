876b36de5a997dd9c56691277053f209367e5d80
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-15 13:33:00 +0300

    Make EngineModel to single

--- MobileWorld.Core/Models/EngineModel.cs
+++ MobileWorld.Core/Models/EngineModel.cs
@@ -1,8 +1,36 @@
-﻿using MobileWorld.Infrastructure.Data.Models;
+﻿using MobileWorld.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MobileWorld.Core.Models
 {
-    public class EngineModel : Engine
+    public class EngineModel 
     {
+        [Required]
+        public FuelType FuelType { get; set; }
+
+        [Required]
+        [Range(40, 2000)]
+        public int HorsePower { get; set; }
+
+        [Range(100, 2000)]
+        public int? NewtonMeter { get; set; }
+
+        [Required]
+        public bool Hybrid { get; set; }
+
+        [Required]
+        [Range(1, 7)]
+        public int EcoLevel { get; set; }
+
+        [Required]
+        public bool AutoGas { get; set; }
+
+        [Required]
+        [Range(900, 9000)]
+        public int CubicCapacity { get; set; }
+
+        [Required]
+        [Range(3, 100)]
+        public double FuelConsuption { get; set; }
     }
 }
