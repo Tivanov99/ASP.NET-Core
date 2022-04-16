c770879299736e768c22fa1f903d5401a1b69ae7
Tivanov99 <trifon.ivanov21@gmail.com>
2022-04-16 11:48:31 +0300

    Add validation attributes to CarInputModel

--- MobileWorld.Core/Models/InputModels/CarInputModel.cs
+++ MobileWorld.Core/Models/InputModels/CarInputModel.cs
@@ -1,6 +1,49 @@
-﻿namespace MobileWorld.Core.Models.InputModels
+﻿using MobileWorld.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MobileWorld.Core.Models.InputModels
 {
     public class CarInputModel
     {
+        public CarInputModel()
+        {
+            Images = new();
+            Features = new();
+        }
+
+        [Required(ErrorMessage = "Полето 'Марка' е задължително!")]
+        public string Make { get; set; }
+
+        //[Required]
+        //[StringLength(20)]
+        //public string Model { get; set; }
+
+        [Range(1886, 2022, ErrorMessage = "Годината трябва да е в диапазона от 1886 до 2022")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Полето 'Скоростна кутия' е задължително!")]
+        public GearType GearType { get; set; }
+
+        [Range(0, 2000000, ErrorMessage = "Цената трябва да е в диапазона от 0 до 2000000")]
+        public decimal MaxPrice { get; set; }
+
+        public EngineModel Engine { get; set; }
+
+        [Required(ErrorMessage = "Полето 'Цвят' е задължително!")]
+        [StringLength(35,MinimumLength =3, ErrorMessage ="Цветът трябва е с дължина от 3 до 35 символа!")]
+        public string Color { get; set; }
+
+        [Required]
+        [Range(2, 12, ErrorMessage = "Трябва да е число в диапазона от 2 до 12.")]
+        public int SeatsCount { get; set; }
+
+        [Required(ErrorMessage = "Полето 'Пробег' е задължително!")]
+        [Range(0,double.MaxValue, ErrorMessage = "Пробегът трябва да е в диапазона от 0 до {2}")]
+        public decimal Mileage { get; set; }
+
+        [Required(ErrorMessage ="Снимките са задължителни!")]
+        public List<byte[]> Images { get; set; }
+
+        public FeaturesModel Features { get; set; }
     }
 }
