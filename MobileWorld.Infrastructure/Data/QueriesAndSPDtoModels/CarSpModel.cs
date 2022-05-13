20e094de126af2f2c42ced5f0e411bca6582becc
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-13 16:22:48 +0300

    Sync

---
+++ MobileWorld.Infrastructure/Data/QueriesAndSPDtoModels/CarSpModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MobileWorld.Infrastructure.Data.QueriesAndSPDtoModels
+{
+    public class CarSpModel
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int GearType { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public int SeatsCount { get; set; }
+        public decimal Mileage { get; set; }
+
+
+        //public AdSpModel Ad { get; set; }
+
+        //[ForeignKey(nameof(Ad))]
+
+        //public string? AdId { get; set; }
+    }
+}
