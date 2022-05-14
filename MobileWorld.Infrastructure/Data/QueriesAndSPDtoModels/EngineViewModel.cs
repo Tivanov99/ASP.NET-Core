ee6659ee71c73f877f9ea5ceb1d7719c5ba27ed8
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-14 16:03:26 +0300

    New item EngineViewModel.cs

---
+++ MobileWorld.Infrastructure/Data/QueriesAndSPDtoModels/EngineViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MobileWorld.Infrastructure.Data.QueriesAndSPDtoModels
+{
+    public class EngineViewModel
+    {
+        public int FuelType { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public int? NewtonMeter { get; set; }
+
+        public int EcoLevel { get; set; }
+
+        public int CubicCapacity { get; set; }
+
+        public double FuelConsuption { get; set; }
+    }
+}
