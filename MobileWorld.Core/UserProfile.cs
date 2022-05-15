6a5310804efda1505fd03d81103edf39591ee8a1
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-15 18:28:37 +0300

    Added new mapping between FeatureSpModel and each of other view features

--- MobileWorld.Core/UserProfile.cs
+++ MobileWorld.Core/UserProfile.cs
@@ -13,9 +13,17 @@ namespace MobileWorld.Core
             CreateMap<AdInfoSpModel, AdViewModel>();
             CreateMap<AdInfoSpModel, RegionViewModel>()
                 .ForMember(pts => pts.Neiborhood, opt => opt.MapFrom(ps => ps.Neiborhood))
-                  .ForMember(pts => pts.TownName, opt => opt.MapFrom(ps => ps.TownName))
-                  .ForMember(pts => pts.RegionName, opt => opt.MapFrom(ps => ps.RegionName));
-            
+                .ForMember(pts => pts.TownName, opt => opt.MapFrom(ps => ps.TownName))
+                .ForMember(pts => pts.RegionName, opt => opt.MapFrom(ps => ps.RegionName));
+
+            CreateMap<FeatureSpModel, ComfortDetailViewModel>();
+            CreateMap<FeatureSpModel, ExteriorDetailViewModel>();
+            CreateMap<FeatureSpModel, InteriorDetailViewModel>();
+            CreateMap<FeatureSpModel, OthersDetailViewModel>();
+            CreateMap<FeatureSpModel, ProtectionDetailViewModel>();
+            CreateMap<FeatureSpModel, SafetyDetailViewModel>();
+
+
             //CreateMap<AdSpModel, AdViewModel>()
             //    .ForMember(pts => pts.Car, opt => opt.MapFrom(ps => ps.Car))
             //    .ForMember(pts => pts.Car.Engine, opt => opt.MapFrom(ps => ps.Engine))
@@ -27,19 +35,19 @@ namespace MobileWorld.Core
             //    .ForMember(pts => pts.Id, opt => opt.MapFrom(ps => ps.AdInfo.Id));
 
 
-                //CreateMap<AdInfoSpModel, AdViewModel>()
-                //    .ForPath(pts => pts.Car.Color, opt => opt.MapFrom(ps => ps.Color))
-                //    .ForPath(pts => pts.Car.GearType, opt => opt.MapFrom(ps => ps.GearType))
-                //    .ForPath(pts => pts.Car.Make, opt => opt.MapFrom(ps => ps.Make))
-                //    .ForPath(pts => pts.Car.Mileage, opt => opt.MapFrom(ps => ps.Mileage))
-                //    .ForPath(pts => pts.Car.SeatsCount, opt => opt.MapFrom(ps => ps.SeatsCount))
-                //    .ForPath(pts => pts.Car.Year, opt => opt.MapFrom(ps => ps.Year))
-                //    .ForPath(pts => pts.Car.Engine.FuelType, opt => opt.MapFrom(ps => ps.FuelType))
-                //    .ForPath(pts => pts.Car.Engine.CubicCapacity, opt => opt.MapFrom(ps => ps.CubicCapacity))
-                //    .ForPath(pts => pts.Car.Engine.NewtonMeter, opt => opt.MapFrom(ps => ps.NewtonMeter))
-                //    .ForPath(pts => pts.Car.Engine.EcoLevel, opt => opt.MapFrom(ps => ps.EcoLevel))
-                //    .ForPath(pts => pts.Car.Engine.FuelConsuption, opt => opt.MapFrom(ps => ps.FuelConsuption))
-                //    .ForPath(pts => pts.Car.Engine.HorsePower, opt => opt.MapFrom(ps => ps.HorsePower));
+            //CreateMap<AdInfoSpModel, AdViewModel>()
+            //    .ForPath(pts => pts.Car.Color, opt => opt.MapFrom(ps => ps.Color))
+            //    .ForPath(pts => pts.Car.GearType, opt => opt.MapFrom(ps => ps.GearType))
+            //    .ForPath(pts => pts.Car.Make, opt => opt.MapFrom(ps => ps.Make))
+            //    .ForPath(pts => pts.Car.Mileage, opt => opt.MapFrom(ps => ps.Mileage))
+            //    .ForPath(pts => pts.Car.SeatsCount, opt => opt.MapFrom(ps => ps.SeatsCount))
+            //    .ForPath(pts => pts.Car.Year, opt => opt.MapFrom(ps => ps.Year))
+            //    .ForPath(pts => pts.Car.Engine.FuelType, opt => opt.MapFrom(ps => ps.FuelType))
+            //    .ForPath(pts => pts.Car.Engine.CubicCapacity, opt => opt.MapFrom(ps => ps.CubicCapacity))
+            //    .ForPath(pts => pts.Car.Engine.NewtonMeter, opt => opt.MapFrom(ps => ps.NewtonMeter))
+            //    .ForPath(pts => pts.Car.Engine.EcoLevel, opt => opt.MapFrom(ps => ps.EcoLevel))
+            //    .ForPath(pts => pts.Car.Engine.FuelConsuption, opt => opt.MapFrom(ps => ps.FuelConsuption))
+            //    .ForPath(pts => pts.Car.Engine.HorsePower, opt => opt.MapFrom(ps => ps.HorsePower));
 
         }
     }
