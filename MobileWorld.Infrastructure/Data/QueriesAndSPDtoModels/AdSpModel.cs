4bd6244efeb9e464f62a54981ad8e05a42636df0
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-15 13:32:51 +0300

    Added AdSpModel.cs which will contains all of results from stored procedures

---
+++ MobileWorld.Infrastructure/Data/QueriesAndSPDtoModels/AdSpModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MobileWorld.Infrastructure.Data.QueriesAndSPDtoModels
+{
+    public class AdSpModel
+    {
+        public AdInfoSpModel AdInfo { get; set; }
+
+        public CarSpModel Car { get; set; }
+
+        public EngineSpModel Engine { get; set; }
+
+        public FeatureSpModel Feature { get; set; }
+
+        public List<string> Images { get; set; }
+    }
+}
