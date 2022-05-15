a6a7afca7f041a8cc9c8c5b504fa63cc4fcce2f8
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-15 12:35:20 +0300

    Added CarSpModel.cs

---
+++ MobileWorld.Infrastructure/Data/QueriesAndSPDtoModels/CarSpModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MobileWorld.Infrastructure.Data.QueriesAndSPDtoModels
+{
+    public class CarSpModel
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public int GearType { get; set; }
+
+        public string Color { get; set; }
+
+        public int SeatsCount { get; set; }
+
+        public decimal Mileage { get; set; }
+    }
+}
