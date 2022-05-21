335323a3d50109c67b7e894efd2106445e6e5e7c
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-21 16:07:14 +0300

    Add implementation to IFeatureViewModel

--- MobileWorld.Core/ViewModels/Contacts/IFeatureViewModel.cs
+++ MobileWorld.Core/ViewModels/Contacts/IFeatureViewModel.cs
@@ -4,5 +4,15 @@
     {
         public IComfortDetailViewModel ComfortDetails { get; set; }
 
+        public IExteriorDetailViewModel ExteriorDetails { get; set; }
+
+        public IInteriorDetailViewModel InteriorDetails { get; set; }
+
+        public IOthersDetailViewModel OthersDetails { get; set; }
+
+        public IProtectionDetailViewModel ProtectionDetails { get; set; }
+
+        public ISafetyDetailViewModel SafetyDetails { get; set; }
+
     }
 }
--- MobileWorld.Core/ViewModels/FeatureViewModel.cs
+++ MobileWorld.Core/ViewModels/FeatureViewModel.cs
@@ -1,19 +1,19 @@
-﻿using MobileWorld.Core.ViewModels.FeatureDetailModels;
+﻿using MobileWorld.Core.ViewModels.Contacts;
 
 namespace MobileWorld.Core.ViewModels
 {
-    public class FeatureViewModel
+    public class FeatureViewModel : IFeatureViewModel
     {
-        public ComfortDetailViewModel ComfortDetails { get; set; }
+        public IComfortDetailViewModel ComfortDetails { get; set; }
 
-        public ExteriorDetailViewModel ExteriorDetails { get; set; }
+        public IExteriorDetailViewModel ExteriorDetails { get; set; }
 
-        public InteriorDetailViewModel InteriorDetails { get; set; }
+        public IInteriorDetailViewModel InteriorDetails { get; set; }
 
-        public OthersDetailViewModel OthersDetails { get; set; }
+        public IOthersDetailViewModel OthersDetails { get; set; }
 
-        public ProtectionDetailViewModel ProtectionDetails { get; set; }
+        public IProtectionDetailViewModel ProtectionDetails { get; set; }
 
-        public SafetyDetailViewModel SafetyDetails { get; set; }
+        public ISafetyDetailViewModel SafetyDetails { get; set; }
     }
 }
