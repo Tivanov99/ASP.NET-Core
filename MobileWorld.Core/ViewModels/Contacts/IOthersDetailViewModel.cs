7fcc9a4369c9791eda5e1e62657ea3c4abd40e91
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-21 16:00:40 +0300

    OthersDetailViewModel inherits IOthersDetailViewModel

--- MobileWorld.Core/ViewModels/Contacts/IOthersDetailViewModel.cs
+++ MobileWorld.Core/ViewModels/Contacts/IOthersDetailViewModel.cs
@@ -1,8 +1,6 @@
-﻿using MobileWorld.Core.ViewModels.Contacts;
-
-namespace MobileWorld.Core.ViewModels.FeatureDetailModels
+﻿namespace MobileWorld.Core.ViewModels.Contacts
 {
-    public class OthersDetailViewModel : IOthersDetailViewModel
+    public interface IOthersDetailViewModel
     {
         public bool AllDrive { get; set; }
 
